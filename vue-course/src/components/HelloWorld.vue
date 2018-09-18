0d47ac4810e30305754de5e4eb5f5ca5cf60f683
Susana Cardenas Molinar <bigdatasuecarmol@pop-os.localdomain>
2018-09-17 23:56:26 -0500

    Initial commit

---
+++ vue-course/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+     <h1>Welcome to the forum</h1>
+     <div v-for="thread in threads">
+       <h2>{{thread.title}}</h2>
+       <div v-for="postId in thread.posts">
+         <p>{{users[posts[postId].userId].name}}</p>
+         <p>{{posts[postId].text}}</p>
+       </div>
+     </div>
+    </div>
+</template>
+
+<script>
+import sourceData from '@/data'
+console.log(sourceData)
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users
+    }
+  }
+}
+</script>
